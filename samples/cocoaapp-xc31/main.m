914ecafcba3cfba6e8e6e60988b15ded991ff9a9
Frédéric Devernay <frederic.devernay@m4x.org>
2016-04-04 18:02:23 +0200

    add sample Xcode 3.1 project building for 10.4

---
+++ samples/cocoaapp-xc31/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  cocoaapp-xc31
+//
+//  Created by Frédéric Devernay on 04/04/16.
+//  Copyright INRIA 2016. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
